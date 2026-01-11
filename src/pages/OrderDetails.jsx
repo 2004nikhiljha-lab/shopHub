3b3f207627d0614a5a7017ae7ff566202ad31237
Nikhil kumar jha <2004nikhiljha@gmail.com>
2026-01-11 12:09:02 +0530

    Update OrderDetails.jsx

--- src/pages/OrderDetails.jsx
+++ src/pages/OrderDetails.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
-import API from "../api"; // ✅ Import the configured API instance
+import API from '../services/api';
 import formatCurrency from "../utils/formatCurrency";
 import { 
   ArrowLeft, 
